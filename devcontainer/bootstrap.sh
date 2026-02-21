fb31a7fbce8e36291cf51bc5dbdb5d1a97e69ec6
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2026-02-21 20:22:16 +0900

    fix: install starship

--- devcontainer/bootstrap.sh
+++ devcontainer/bootstrap.sh
@@ -11,7 +11,7 @@ fi
 # Install starship
 if ! command -v starship &> /dev/null; then
   echo "🚀 Installing Starship for my personal use..."
-  curl -sS https://starship.rs/install.sh | sh
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
 fi
 
 # 2. 既存の install.sh を実行 (シンボリックリンクの作成)
