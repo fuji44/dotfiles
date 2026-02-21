04431695fd61b9627835fff04fd389b894b63e00
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2026-02-21 18:42:30 +0900

    fix: bootstrap

--- devcontainer/bootstrap.sh
+++ devcontainer/bootstrap.sh
@@ -4,12 +4,8 @@ set -eu
 # Install sheldon
 if ! command -v sheldon &> /dev/null; then
     echo "🚀 Installing Sheldon for my personal use..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    # パスを一時的に通して直後の実行を可能にする
-    export PATH="$HOME/.local/bin:$PATH"
-    # sheldon のインストール (cargo等を使用するかバイナリを落とす)
-    # ここでは例としてバイナリを直接取得する公式スクリプトを想定
-    curl --proto '=https' --tlsv1.2 -sSf https://sheldon.cli.rs/install.sh | sh -s -- --bin-dir "$HOME/.local/bin"
+    curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
 fi
 
 # 2. 既存の install.sh を実行 (シンボリックリンクの作成)
