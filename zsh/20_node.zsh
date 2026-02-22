0d10de877cca5b59bc07d32f0a14acdb2080b74a
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2026-02-22 14:42:38 +0900

    fix: remove pnpm settings

--- zsh/20_node.zsh
+++ zsh/20_node.zsh
@@ -23,21 +23,3 @@ NPM_GLOBAL_DIR="$HOME/.npm-global"
 if [ -d "$NPM_GLOBAL_DIR" ]; then
   export PATH="$NPM_GLOBAL_DIR/bin:$PATH"
 fi
-
-# npm
-# if command -v npm >/dev/null 2>&1; then
-#   alias ni="npm install"
-#   alias nD="npm install -D"
-#   alias nr="npm run"
-#   alias ns="npm start"
-#   alias nt="npm test"
-# fi
-
-# pnpm
-if command -v pnpm >/dev/null 2>&1; then
-  export PNPM_HOME="$HOME/.local/share/pnpm"
-  case ":$PATH:" in
-    *":$PNPM_HOME:"*) ;;
-    *) export PATH="$PNPM_HOME:$PATH" ;;
-  esac
-fi
